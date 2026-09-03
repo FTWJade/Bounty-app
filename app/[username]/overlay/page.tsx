0f69d9470c23a9c77982cee9aea31880f45dd67d
FTWJade <47994603+FTWJade@users.noreply.github.com>
2026-09-02 22:04:34 -0600

    Hide unable-to-vote message from OBS overlay

--- app/[username]/overlay/page.tsx
+++ app/[username]/overlay/page.tsx
@@ -259,6 +259,7 @@ export default function UserOverlay({
                             getUserColor={getUserColor}
                             myVote={null}
                             pendingVote={null}
+                            canVote={true}
                         />
                     </>
                 )}
